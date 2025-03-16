93cba9820fa8388c74c96e95b822832fa25f068b
zainabbasi38 <zainabbasi7534@gmail.com>
2025-03-16 16:04:42 +0300

    vaiables

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+
+let a = 3;
+let b = 3;
+let c = 3;
+
+
+console.log("hey , i ma learning javascript");
+console.log(a + b + c);
+
+let z = 3;
+let y = "hello zain";
+let x = true;
+let w = null;
+let q = undefined
+
+console.log(z, y ,x, w , q)
+console.log(typeof z, typeof y ,typeof x, typeof w , typeof q)
+
+const info = {
+    "name": "zain",
+    "age": 19
+};
+
+console.log(info)
+info.education = "intermediate"
+console.log(info)
+info.education = "inter"
+console.log(info)
+
+
+
+
