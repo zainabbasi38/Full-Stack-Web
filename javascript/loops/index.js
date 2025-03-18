25aa7c145ee40b8fafa3076bbf6e3ff060dc30c1
zainabbasi38 <zainabbasi7534@gmail.com>
2025-03-18 15:37:14 +0300

    loops

---
+++ javascript/loops/index.js
@@ -0,0 +1,52 @@
+console.log("Hey i am learning loops in java-script")
+
+
+// let i = 0
+// for (let i = 0; i < 20; i++) {
+//     console.log(i)    
+// }
+
+
+// let obj = {
+//     name: "Zain",
+//     age: 19,
+//     education: "inter"
+// };
+
+// for ( key in obj) {
+//         const element = obj[key];
+//         console.log(key, element)
+        
+//     }
+
+
+// let name = "ZAIN ABBASI"
+// for (const element of name) {
+//     console.log(element)
+    
+// }
+
+// let a= 0
+// while (a < 100) {
+//     console.log(a);
+//     a++;
+    
+// }
+
+let student_marks = {
+    zain: 90,
+    ali : 92,
+    saad: 86
+};
+
+// for (const key in student_marks) {
+//         const element = student_marks[key];
+//         console.log(element)
+        
+    
+// }
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
